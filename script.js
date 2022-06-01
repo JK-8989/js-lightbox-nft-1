ba50002c63a29a5369e855453a9a57c5a6108537
JK-8989 <jiyoungkim365@gmail.com>
2022-06-01 12:52:19 -0700

    Add full image src using JS

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const overlay = document.getElementById('lightbox-overlay');
+const images = document.querySelectorAll('li > img');
+const fullImage = document.getElementById('full-image');
+console.log(fullImage)
 
+images.forEach((images) => {
+  images.addEventListener('click', ()=>{
+    overlay.classList.add('active');
 
-console.log('connected')
+      let target = images.target,
+          targetId = images.getAttribute('src');
+          console.log(targetId)
+          fullImage.src = targetId;
+
+   
+
+    overlay.addEventListener('click', ()=>{
+      overlay.classList.remove('active');
+    })
+  })
+})
+
+console.log(images)
